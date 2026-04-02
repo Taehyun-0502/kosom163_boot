2039a8a9da56570f42b0f5d8d04d0cce0e33df93
Taehyun-0502 <uiop72323@gmail.com>
2026-04-02 13:07:29 +0900

    init

---
+++ src/test/java/com/naver/sth/Kosmo163BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naver.sth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kosmo163BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
